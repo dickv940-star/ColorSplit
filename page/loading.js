fd5df06b8a62a6783e6788b39cf509db9c850ef9
dickv940-star <dickv940@gmail.com>
2026-07-30 10:37:45 +0700

    Create loading.js

---
+++ page/loading.js
@@ -0,0 +1,27 @@
+function renderLoading(){
+
+document.getElementById("app").innerHTML=`
+
+<div class="loading">
+
+<h2>Processing File</h2>
+
+<div class="spinner"></div>
+
+<p id="loadingText">
+
+Reading File...
+
+</p>
+
+</div>
+
+`;
+
+setTimeout(()=>{
+
+showPage("workspace");
+
+},2000);
+
+}
