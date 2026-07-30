2fa2415eca3bbc3eae180c97c004b872975c715e
dickv940-star <dickv940@gmail.com>
2026-07-30 11:57:06 +0700

    Update colorDetector.js

--- js/engines/colorDetector.js
+++ js/engines/colorDetector.js
@@ -1,6 +1,7 @@
 /*
 =========================================================
 Color Detector
+Version 2.0
 =========================================================
 */
 
@@ -13,13 +14,62 @@ const ColorDetector={
         if(!AppState.image)
             return;
 
-        AppState.metadata.colorSpace="RGB";
+        const canvas=document.createElement("canvas");
 
-        if(AppState.metadata.alpha){
+        canvas.width=AppState.image.width;
 
-            AppState.metadata.colorSpace+=" + Alpha";
+        canvas.height=AppState.image.height;
+
+        const ctx=canvas.getContext("2d");
+
+        ctx.drawImage(AppState.image,0,0);
+
+        const img=ctx.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        ).data;
+
+        let gray=true;
+
+        let alpha=false;
+
+        for(let i=0;i<img.length;i+=4){
+
+            const r=img[i];
+
+            const g=img[i+1];
+
+            const b=img[i+2];
+
+            const a=img[i+3];
+
+            if(r!==g||g!==b){
+
+                gray=false;
+
+            }
+
+            if(a<255){
+
+                alpha=true;
+
+            }
 
         }
+
+        let space="RGB";
+
+        if(gray)
+            space="Grayscale";
+
+        if(alpha)
+            space+=" + Alpha";
+
+        AppState.metadata.colorSpace=space;
+
+        AppState.metadata.alpha=alpha;
 
     }
 
