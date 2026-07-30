fcbdcf3099e3ae037117127603404f414a6fb6a1
dickv940-star <dickv940@gmail.com>
2026-07-30 13:47:49 +0700

    Create textDetector.js

---
+++ js/emgines/freehand/textDetector.js
@@ -0,0 +1,52 @@
+/*
+=========================================================
+Text Detector Engine
+=========================================================
+*/
+
+
+const TextDetector = {
+
+
+
+detect(){
+
+
+return {
+
+
+type:
+"text",
+
+
+content:
+"",
+
+
+font:
+"unknown",
+
+
+size:
+12,
+
+
+color:
+"#000000"
+
+
+
+};
+
+
+
+}
+
+
+
+};
+
+
+
+window.TextDetector =
+TextDetector;
