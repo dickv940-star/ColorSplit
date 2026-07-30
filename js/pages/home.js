b9489102aae68ea9b65b9326abe1cf6912416264
dickv940-star <dickv940@gmail.com>
2026-07-30 11:06:32 +0700

    Update home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,43 +1,22 @@
-function renderHome(){
+document.getElementById("fileInput").onchange = async function () {
 
-document.getElementById("app").innerHTML = `
-
-<div class="home">
-
-<img src="assets/logo.png" class="logo">
-
-<h1>ColorSplit Pro</h1>
-
-<p>
-Automatic CMYK Separation
-</p>
-
-<label class="upload">
-
-<input
-type="file"
-id="fileInput"
-accept=".pdf,.png,.jpg,.jpeg,.tif,.tiff">
-
-Upload File
-
-</label>
-
-</div>
-
-`;
-
-document
-.getElementById("fileInput")
-.onchange=async function(){
-
-    if(!this.files.length)
+    if (!this.files.length)
         return;
 
     showPage("loading");
 
-    await importFile(this.files[0]);
+    const file = this.files[0];
+
+    await FileReaderEngine.open(file);
+
+    await MetadataReader.read(file);
+
+    await ImageLoader.load(file);
+
+    await ColorDetector.detect();
 
     showPage("workspace");
 
-}
+    PreviewLoader.draw();
+
+};
